0d791c5e0b0bc8c6b185b5907e8719cf0f9ce16b
suinua <maatinosippo@gmail.com>
2019-04-13 19:24:53 +0900

    初期コミット

---
+++ lib/models/friend.dart
@@ -0,0 +1,7 @@
+import 'package:pokeep/models/user.dart';
+
+class Friend implements User {
+  @override
+  String name;
+  String mail;
+}
---
+++ lib/models/me.dart
@@ -0,0 +1,9 @@
+import 'package:pokeep/models/user.dart';
+
+class Me implements User {
+  @override
+  String name;
+  String main;
+  String password;
+
+}
