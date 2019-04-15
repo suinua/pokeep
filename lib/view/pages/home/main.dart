7a275654abbefbf786cde58a23aa15ec131f84d7
suinua <maatinosippo@gmail.com>
2019-04-15 16:16:50 +0900

    [Update] AppBarにアイコンが表示されるように

--- lib/view/pages/home/main.dart
+++ lib/view/pages/home/main.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:pokeep/blocs/me_bloc/main.dart';
+import 'package:pokeep/blocs/me_bloc/provider.dart';
+import 'package:pokeep/models/me.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final MeBloc meBloc = MeBlocProvider.of(context);
+
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Home'),
+        actions: <Widget>[
+          StreamBuilder<Me>(
+              stream: meBloc.me,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return Container();
+
+                return Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(60.0),
+                        child: Image.network(snapshot.data.iconUrl),
+                      ),
+                    ),
+                  ),
+                );
+              })
+        ],
+      ),
       body: Container(),
     );
   }
