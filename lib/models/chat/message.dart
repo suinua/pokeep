457f8a8501be982b9fdde21002554c676d7a7bd0
suinua <maatinosippo@gmail.com>
2019-04-15 17:50:08 +0900

    clean

--- lib/models/chat/message.dart
+++ lib/models/chat/message.dart
@@ -1,13 +1,13 @@
 import 'package:pokeep/models/account/user.dart';
 
 class Message {
-  final User sendUser;
+  final User owner;
   final DateTime sendDate;
   final String image;
 
   bool hasImage() => image != null;
 
-  Message({this.sendUser, this.sendDate}) : image = null;
+  Message({this.owner, this.sendDate}) : image = null;
 
-  Message.withImage({this.sendUser, this.sendDate, this.image});
+  Message.withImage({this.owner, this.sendDate, this.image});
 }
