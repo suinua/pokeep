65386babd2c1ba4e07759b1ba31f79284b552aed
suinua <maatinosippo@gmail.com>
2019-04-15 18:13:36 +0900

    Update

--- lib/models/chat/message.dart
+++ lib/models/chat/message.dart
@@ -1,13 +1,49 @@
+import 'package:meta/meta.dart';
 import 'package:pokeep/models/account/user.dart';
 
 class Message {
-  final User owner;
-  final DateTime sendDate;
-  final String image;
+  String key;
+  User _owner;
+  DateTime _sendDate;
+  String _image;
 
-  bool hasImage() => image != null;
+  User get owner => _owner;
 
-  Message({this.owner, this.sendDate}) : image = null;
+  DateTime get sendDate => _sendDate;
 
-  Message.withImage({this.owner, this.sendDate, this.image});
+  String get image => _image;
+
+  bool hasImage() => _image != null;
+
+  Message({
+    @required
+    User owner,
+    @required
+    DateTime sendDate,
+  })  : _owner = owner,
+        _sendDate = sendDate,
+        _image = null;
+
+  Message.withImage({
+    @required
+    User owner,
+    @required
+    DateTime sendDate,
+    @required
+    String image,
+  })  : _owner = owner,
+        _sendDate = sendDate,
+        _image = image;
+
+  Message.fromJson(this.key, Map<String, dynamic> value) {
+    _owner = value['owner'];
+    _sendDate = value['send_date'];
+    _image = value['image'];
+  }
+
+  Map<String, dynamic> toJson() => {
+        'owner': _owner,
+        'send_date': _sendDate,
+        'image': _image,
+      };
 }
