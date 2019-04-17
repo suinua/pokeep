ce6d02fc324991f21e96fbfd09cf83d3f32a10e9
suinua <maatinosippo@gmail.com>
2019-04-17 14:05:52 +0900

    Add provider

---
+++ lib/blocs/chat_blocs/chat_group_bloc/provider.dart
@@ -0,0 +1,25 @@
+import 'package:bloc_provider/bloc_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+import 'package:pokeep/blocs/chat_blocs/chat_group_bloc/bloc.dart';
+
+class ChatGroupBlocProvider extends BlocProvider<ChatGroupBloc> {
+  ChatGroupBlocProvider({
+    @required ChatGroupBloc value,
+  }) : super(
+    creator: (context, _bag) {
+      assert(context != null);
+      return value;
+    },
+  );
+
+  ChatGroupBlocProvider.fromBlocContext({
+    @required BuildContext context,
+    @required Widget child,
+  }) : super.fromBlocContext(
+    child: child,
+    context: context,
+  );
+
+  static ChatGroupBloc of(BuildContext context) => BlocProvider.of(context);
+}
