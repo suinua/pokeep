220d890343fdcfcf44b90cb0817f5cee78d5308f
suinua <maatinosippo@gmail.com>
2019-04-17 19:54:31 +0900

    Add my_items_bloc

---
+++ lib/blocs/my_items_bloc/bloc.dart
@@ -0,0 +1,9 @@
+import 'package:bloc_provider/bloc_provider.dart';
+import 'package:pokeep/blocs/joining_chat_groups_blocs/bloc.dart';
+
+class MyItemsBloc extends Bloc {
+  JoiningChatGroupsBloc joiningChatGroupsBloc;
+
+  @override
+  void dispose() {}
+}
