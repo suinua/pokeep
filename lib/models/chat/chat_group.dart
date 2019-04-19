ac3cdb6c4302e17a0a55db166e95bd008c8d396e
suinua <maatinosippo@gmail.com>
2019-04-19 18:07:15 +0900

    ChatGroupが子要素のBlocを持つように

--- lib/models/chat/chat_group.dart
+++ lib/models/chat/chat_group.dart
@@ -1,11 +1,19 @@
+import 'package:pokeep/blocs/joining_chat_groups_blocs/chat_group_members_bloc/bloc.dart';
+import 'package:pokeep/blocs/joining_chat_groups_blocs/chat_group_messages_bloc/bloc.dart';
+
 class ChatGroup {
   String key;
   String name;
 
+  ChatGroupMessagesBloc messagesBloc;
+  ChatGroupMembersBloc membersBloc;
+
   ChatGroup(this.name);
 
   ChatGroup.fromJson(this.key, Map<String, dynamic> value) {
-    name = value['name'];
+    this.name = value['name'];
+    messagesBloc = ChatGroupMessagesBloc(this.key);
+    membersBloc = ChatGroupMembersBloc(this.key);
   }
 
   Map<String, dynamic> toJson() => {
