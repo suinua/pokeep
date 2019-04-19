d33e89cdbf6442fbddd0310e691b438584c1de27
suinua <maatinosippo@gmail.com>
2019-04-19 19:22:15 +0900

    Clean

--- lib/blocs/joining_chat_groups_blocs/chat_group_messages_bloc/bloc.dart
+++ lib/blocs/joining_chat_groups_blocs/chat_group_messages_bloc/bloc.dart
@@ -15,9 +15,14 @@ class ChatGroupMessagesBloc extends Bloc {
 
   Stream<List<Message>> get getMessages => _messagesController.stream;
 
-  BehaviorSubject<Message> _addMessageController = BehaviorSubject<Message>();
+  BehaviorSubject<Message> _sendMessageController = BehaviorSubject<Message>();
+
+  Sink<Message> get send => _sendMessageController.sink;
+
   BehaviorSubject<Message> _deleteMessageController =
       BehaviorSubject<Message>();
+
+  Sink<Message> get delete => _deleteMessageController.sink;
 
   ChatGroupMessagesBloc(this.groupKey) {
     _repository = ChatGroupMessagesRepository(
@@ -31,7 +36,7 @@ class ChatGroupMessagesBloc extends Bloc {
         _setMessages.add(_messages);
       },
     );
-    _addMessageController.stream.listen((Message message) {
+    _sendMessageController.stream.listen((Message message) {
       _repository.addMessage(message);
     });
     _deleteMessageController.stream.listen((Message message) {
@@ -43,7 +48,7 @@ class ChatGroupMessagesBloc extends Bloc {
   void dispose() async {
     _repository.dispose();
     await _messagesController.close();
-    await _addMessageController.close();
+    await _sendMessageController.close();
     await _deleteMessageController.close();
   }
 }
